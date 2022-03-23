1d034c05372354a0fe95432e2498ba41b9064f38
David Wilson <david.wilson.coding@gmail.com>
2022-03-23 22:15:22 +0000

    Imported mongoose models and updated app routing

--- index.js
+++ index.js
@@ -11,260 +11,201 @@ const uuid = require('uuid');
 //load bodyParser
 const bodyParser = require('body-parser');
 
+
+//load mongoose
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+//import data models
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
 //log request data in terminal
 app.use(morgan('common'));
 
 //allow request body to be parsed
 app.use(bodyParser.json());
 
-let movies = [
-  {
-    title: 'Green Mile',
-    genre: {
-      genreName: "drama",
-      genreDesc: "a written work that tells a story through action and speech and is acted out.",
-    },
-    director: {
-      dirName: 'Frank Darabont'
-    },    
-    star: 'Tom Hanks',
-    year: 2000
-  },
-  {
-    title: 'The Brothers Grimsby',
-    genre: {
-      genreName: "comedy",
-      genreDesc: "a genre designed to cause amusement."
-    },
-    director: {
-      dirName: 'Louis Leterrier'
-    },
-    star: 'Sascha Baron Cohen',
-    year: 2016
-  },
-  {
-    title: 'In the Name of the Father',
-    genre: {
-      genreName: "drama",
-      genreDesc: "a written work that tells a story through action and speech and is acted out.",
-    },
-    director: {
-      dirName: 'Jim Sheridan'
-    },
-    star: 'Daniel Day-Lewis',
-    year: 1993
-  },
-  {
-    title: 'Ferris Bueller\'s Day Off',
-    genre: {
-      genreName: "comedy",
-      genreDesc: "a genre designed to cause amusement."
-    },
-    director: {
-      dirName: 'John Hughes'
-    },
-    star: 'Matthew BRoderick',
-    year: 1986
-  },
-  {
-    title: 'Drive',
-    genre: {
-      genreName: "drama",
-      genreDesc: "a written work that tells a story through action and speech and is acted out.",
-    },
-    director: {
-      dirName: 'Louis Leterrier'
-    },
-    star: 'Ryan Gosling',
-    year: 2011
-  },
-  {
-    title: 'Borat',
-    genre: {
-      genreName: "comedy",
-      genreDesc: "a genre designed to cause amusement."
-    },
-    director: {
-      dirName: 'Larry Charles'
-    },
-    star: 'Sascha Baron Cohen',
-    year: 2006
-  },
-  {
-    title: 'In the Light of the Moon',
-    genre: {
-      genreName: "horror",
-      genreDesc: "a story in which the focus is on creating a feeling of fear."
-    },
-    director: {
-      dirName: 'Leah Welch'
-    },
-    star: 'Hailee Lipscomb',
-    year: 2021
-  },
-  {
-    title: 'The Shining',
-    genre: {
-      genreName: "horror",
-      genreDesc: "a story in which the focus is on creating a feeling of fear."
-    },
-    director: {
-      dirName: 'Stanley Kubrick'
-    },
-    star: 'Jack Nicholson',
-    year: 1980
-  },
-  {
-    title: 'The Godfather',
-    genre: {
-      genreName: "drama",
-      genreDesc: "a written work that tells a story through action and speech and is acted out.",
-    },
-    director: {
-      dirName: 'Franics Ford Coppola'
-    },
-    star: 'Al Pacino',
-    year: 1972
-  },
-  {
-    title: 'Scent of a Woman',
-    genre: {
-      genreName: "drama",
-      genreDesc: "a written work that tells a story through action and speech and is acted out.",
-    },
-    director: {
-      dirName: 'Martin Brest'
-    },
-    star: 'Al Pacino',
-    year: 1992
-  }
-];
-
-let users =  [
-  {
-    userId: 1,
-    userName: 'David',
-    email: 'david.wilson.coding@gmail.com',
-    password: 'secretPassword',
-    birthday: '30/12/1974',
-    topFilms: ['Borat', 'Drive']
-  }
-]
+//serve static files from the public directory
+app.use(express.static('public'));
 
 /* ********************* */
 /* APP ROUTING */
 /* ********************* */
 
 //GET all movies
-app.get("/movies", (req, res) => {
-  res.status(200).json(movies);
+app.get ('/movies', (req, res) => {
+  Movies.find()
+  .then((movies) => {
+    res.status(200).json(movies);
+  })
+  .catch((err) => {
+    res.status(500).send('Error: ' + err);
+  });
 });
   
 //GET movie data by title
-app.get("/movies/:title", (req, res) => {
-  const { title } = req.params;
-  const movie = movies.find((movie) => movie.title === title);
-  if (movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(400).send("Movie not found");
-  }
+app.get('/movies/:title', (req, res) => {
+  //find the movie by title
+  Movies.find({ Title: req.params.title}) 
+  .then((movie) => {
+    //If movie was found, return data, else generate error
+    if(movie){
+      res.status(200).json(movie);
+    } else {
+      res.status(400).send('Movie not found');
+    };
+  })
+  .catch((err) => {
+    res.status(500).send('Error: '+ err);
+  });
 });
 
-//GET movie genre data
-app.get("/movies/genres/:genreName", (req, res) => {
-  const genre = movies.find(
-    (movie) => movie.genre.genreName === req.params.genreName
-  ).genre;
-  if (genre) {
-    res.status(200).json(genre);
-  } else {
-    res.status(400).send("Genre does not exist");
-  }
+//GET movie genre data by genre name
+app.get('/movies/genres/:Name', (req, res) => {
+  console.log('test1');
+  //find movie by genre name
+  Movies.findOne({ 'Genre.Name': req.params.Name})
+    .then((movie) => {
+      //if a movie with the genre was found, return genre info, else generate an error
+      if(movie){ 
+        res.status(200).json(movie.Genre);
+      } else {
+        res.status(400).send('Genre not found');
+      };
+    })
+    .catch((err) => {
+      res.status(500).send('Error: '+ err);
+    });
+    console.log('test2');
 });
 
-//GET movie director data
-app.get("/movies/directors/:dirName", (req, res) => {
-  const director = movies.find(
-    (movie) => movie.director.dirName === req.params.dirName
-  ).director;
-  if (director) {
-    res.status(200).json(director);
-  } else {
-    res.status(404).send("Director not found.");
-  }
+//GET movie director data by director name
+app.get('/movies/directors/:Name', (req, res) => {
+  //find one movie with the director by name
+  Movies.findOne({ 'Director.Name': req.params.Name}) 
+    .then((movie) => {
+      // If a movie with the director was found, return director data, else generate an error
+      if(movie){ 
+        res.status(200).json(movie.Director);
+      } else {
+        res.status(400).send('Director not found');
+      };
+    })
+    .catch((err) => {
+      res.status(500).send('Error: '+ err);
+    });
 });
 
 //POST new user (to POST new user to users)
 app.post('/users', (req, res) => {
-  const newUser = req.body;
-  if (newUser.userName) {
-    newUser.userId = uuid.v4();
-    users.push(newUser);
-    res.status(201).json(newUser);
-  } else {
-    res.status(400).send("userName not entered");
-  }
+  Users.findOne({Username : req.body.Username})
+  .then((user) => {
+    //if the Username already exists, genrate an error
+    if(user) {
+      return res.status(400).send('User with the Username ' + req.body.Username + ' already exists!')
+      // If the username is not held in database, create a new user from request body
+    } else {
+      Users
+        .create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday
+        })
+        .then((user) => {res.status(201).json(user)})
+        .catch((err) => {
+          console.error(err);
+          res.status(500).send('Error: ' + err);
+        })
+      }
+    })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
 //PUT nuewUserName (to allow users to update userName)
-app.put('/users/:userId', (req, res) => {
-  const reqBody = req.body;
-  const user = users.find((user) => user.userId == pUserId);
-  if (user) {
-    user.userName = reqBody.userName;
-    res.send(user);
-  }
-
+app.put('/users/:Username', (req, res) => {
+  //find user by existing username
+  Users.findOneAndUpdate({ Username : req.params.Username},
+    //update user data from request body 
+    {$set: {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+      }
+    },
+    //Use updated object as callback parameter
+    { new : true }) 
+    .then((updatedUser) => {
+        //return json object of updatedUser
+        res.json(updatedUser); 
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-//POST new movie to user's topFIlms array
-app.post("/users/:userId/:movie", (req, res) => {
-  const pUserId = req.params.userId;
-  const pMovie = req.params.movie;
-  let user = users.find((user) => user.userId == pUserId);
-
-  if (user) {
-    console.log('user found');
-    user.topFilms.push(pMovie);
-    res.status(200).send(`${pMovie} has been added to user ${pUserId}'s array`);
-  } else {
-    res.status(400).send("User does not exist");
-  }
+//POST new movie to user's FavoriteMovies array
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({Username : req.params.Username}, // Find user by username
+    //add movie to FavoriteMovies if not in existing array
+    {$addToSet: { FavoriteMovies: req.params.MovieID}},
+    //use updated object as callback parameter
+    { new : true }) 
+    .then((updatedUser) => {
+        //return json object of updatedUser
+        res.json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 //DELETE movie from user's topFilms array
-app.delete("/users/:userId/:movie", (req, res) => {
-  const pUserId = req.params.userId;
-  const pMovie = req.params.movie;
-  let user = users.find((user) => user.userId == pUserId);
-  if (user) {
-      res.status(200).send(`${pMovie} has been deleted from user ${pUserId}'s array`);
-  } else {
-    res.status(404).send("User does not exist");
-  }
+app.put('/users/:Username/movies/:MovieID', (req, res) => {
+  console.log(req.params.MovieID);
+  //find user by username
+  Users.findOneAndUpdate({Username : req.params.Username}, 
+    //remove movie from the list
+    {$pull: { FavoriteMovies: req.params.MovieID}}, 
+    //use updated object as callback parameter
+    { new : true }) 
+    .then((updatedUser) => {
+        //return json object of updatedUser
+        res.json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 //DELETE user from users
-app.delete("/users/:userId", (req, res) => {
-  const pUserId = req.params.userId;
-  let user = users.find((user) => user.userId == pUserId);
-  if (user) {
-    users = users.filter((user) => {
-      return user.userId !== pUserId;
+app.delete('/users/:Username', (req, res) => {
+  //find user by username
+  Users.findOneAndRemove({ Username : req.params.Username}) 
+    .then((user) => {
+      //if user was found, return success message, else return error
+      if(user){
+        res.status(200).send('User with the Username ' + req.params.Username + ' was sucessfully deleted.');
+      } else {
+        res.status(400).send('User with the Username ' + req.params.Username + ' was not found.');
+      };
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
     });
-    console.log("here");
-    res.status(201).send("User " + pUserId + " was deleted.");
-  } else {
-    res.status(404).send("User does not exist");
-  }
-});
-
-
+  });
   
-//serve static files from the public directory
-app.use(express.static('public'));
-
 //error handling
 app.use((err, req, res, next) => {
 console.error(err.stack)
