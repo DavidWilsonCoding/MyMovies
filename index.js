e93efef6cfcbf1d31e55cd70efc7672408477b1c
David Wilson <david.wilson.coding@gmail.com>
2022-03-31 21:57:21 +0100

    Temporarily removies jwt authentication for /movies endpoint

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.use(express.static('public'));
 
 //passport.authenticate('jwt', { session: false }), 
 //GET all movies
-app.get ('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get ('/movies', { session: false }), (req, res) => {
   Movies.find()
   .then((movies) => {
     res.status(200).json(movies);
